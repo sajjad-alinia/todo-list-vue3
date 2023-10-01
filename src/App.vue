d2ac97814a26414a36b0926dcb20012c97dc73e1
s-sazehhesab <145561531+s-sazehhesab@users.noreply.github.com>
2023-10-01 10:01:22 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,35 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
+  if ('Notification' in window) {
+  const notificationStatus = Notification.permission
+  if (notificationStatus != 'granted') {
+    requestPermission()
+  }
+}
+
+setInterval(() => {
+  const localStoreSRV = new LocalStoreService()
+  const todoList = localStoreSRV.getTodoList()
+  todoList.map((item) => {
+    const dDate = new Date(item.dueDate).getTime()
+    const currentDate = new Date().getTime()
+    const def = dDate - currentDate
+
+    if (def < 86_400_000) {
+      new Notification(item.title, { body: item.description })
+    }
+  })
+}, 20000)
+
+function requestPermission() {
+  if (!('Notification' in window)) {
+    alert('Notification API not supported!')
+    return
+  }
+
+  Notification.requestPermission(function (result) {})
+}
 </script>
 
 <template>
